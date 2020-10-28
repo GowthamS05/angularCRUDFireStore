de4b07ad9f584d56aa520cad2f5629e6ff85ed69
unknown <raogowtham@gmail.com>
2020-10-28 23:26:17 +0530

    Image Upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { AppService } from './app.service';
 import { NgForm } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { Observable } from 'rxjs';
+import { map, finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +15,13 @@ export class AppComponent {
   isUpdated = false;
   model: any = {};
   tableVal: any = [];
-  constructor(public appservice: AppService) {
+  selectedFile: File = null;
+  downloadURL: Observable<string>;
+
+  constructor(
+    public appservice: AppService,
+    private storage: AngularFireStorage
+  ) {
     this.getEmployee();
   }
   getEmployee() {
@@ -26,14 +35,17 @@ export class AppComponent {
           lastName: e.payload.doc.data()['lastName'],
           email: e.payload.doc.data()['email'],
           password: e.payload.doc.data()['password'],
+          imgUrl: e.payload.doc.data()['imgUrl'],
         };
       });
       this.tableVal = emp;
+      console.log(this.tableVal);
     });
   }
 
   onSubmit(f: NgForm) {
     let data = f.form.value;
+    data.imgUrl = this.model.imgUrl;
     if (f.form.valid && !this.isUpdated) {
       this.appservice
         .create_Newemployee(data)
@@ -60,5 +72,29 @@ export class AppComponent {
     this.model.lastName = record.lastName;
     this.model.email = record.email;
     this.model.password = record.password;
+  }
+  onFileSelected(event) {
+    var n = Date.now();
+    const file = event.target.files[0];
+    const filePath = `Flats/${n}`;
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(`Flats/${n}`, file);
+    task
+      .snapshotChanges()
+      .pipe(
+        finalize(() => {
+          this.downloadURL = fileRef.getDownloadURL();
+          this.downloadURL.subscribe((url) => {
+            if (url) {
+              this.model.imgUrl = url;
+            }
+          });
+        })
+      )
+      .subscribe((url) => {
+        if (url) {
+          console.log('status', url);
+        }
+      });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AppComponent } from './app.component';
-
+import { AngularFireStorageModule, BUCKET } from '@angular/fire/storage';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     FormsModule,
     AngularFireModule.initializeApp({
-      apiKey: 'AIzaSyCwc-g9ujUE5toXBPgWWzu76v3WwoC4q0E',
-      authDomain: 'fir-crud-47634.firebaseapp.com',
-      databaseURL: 'https://fir-crud-47634.firebaseio.com',
-      projectId: 'fir-crud-47634',
-      storageBucket: 'fir-crud-47634.appspot.com',
-      messagingSenderId: '748026569909',
-      appId: '1:748026569909:web:a3ba67c777f1435fcbcafc',
+      apiKey: 'test',
+      authDomain: 'test',
+      databaseURL: 'test',
+      projectId: 'test',
+      storageBucket: 'test',
+      messagingSenderId: 'test',
+      appId: 'test',
     }),
-
+    AngularFireStorageModule,
     AngularFirestoreModule,
   ],
-  providers: [],
+  providers: [{ provide: BUCKET, useValue: 'stotrageBucket' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
